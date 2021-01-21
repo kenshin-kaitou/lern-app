a0c09f76418bedcc89b8255e230e5e5b0cbad465
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2021-01-21 18:23:04 +0900

    いいね機能の実装

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -3,4 +3,6 @@ class Lecture < ApplicationRecord
 	has_one_attached :image
 	has_one_attached :video
 	belongs_to :category
+	has_many :likes
+	has_many :liked_users, through: :likes, source: :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'lectures/search'
   root 'lectures#index'
-  resources :lectures, only:[:new,:create,:show,:edit,:update,:destroy]
+  resources :lectures, only:[:new,:create,:show,:edit,:update,:destroy] do
+    resources :likes, only: [:create, :destroy]
+  end
 end
